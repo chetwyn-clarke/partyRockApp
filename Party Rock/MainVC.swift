6dd93e62c518304e1282dabeb3b8a30fc963d0d0
chetwyn-clarke <chetwynclarke@Chetwyn-Clarkes-MacBook.local>
2017-03-26 22:02:54 -0500

    First commit.  Finished Lesson 20

---
+++ Party Rock/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Party Rock
+//
+//  Created by Chetwyn on 3/26/17.
+//  Copyright © 2017 Clarke Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
